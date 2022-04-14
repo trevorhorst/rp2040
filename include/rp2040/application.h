944f6cec2641b9a5392d4e9d5fd3ae0d90521e89
Trevor Horst <trevorhorst1212@gmail.com>
2022-04-14 10:25:39 -0400

    Adding the SSD1306 display driver. Software will perform basic setup.

--- include/rp2040/application.h
+++ include/rp2040/application.h
@@ -6,12 +6,15 @@
 #include "pico/stdlib.h"
 
 #include "rp2040/drivers/ws2812.h"
+#include "rp2040/drivers/ssd1306.h"
 
 class Application
 {
     static const uint32_t neopixel_pin;
     static const uint32_t neopixel_num_leds;
     static const uint32_t neopixel_max_brightness;
+    static const uint32_t i2c1_sda_pin;
+    static const uint32_t i2c1_scl_pin;
     static const uint32_t heartbeat_ms;
 
 public:
@@ -19,9 +22,20 @@ public:
 
     int32_t run();
     void initialize();
+    void initializeI2C();
+    void initializeDisplay();
+
+    bool reserved_addr(uint8_t addr)
+    {
+        return (addr & 0x78) == 0 || (addr & 0x78) == 0x78;
+    }
+
+    void i2cBusScan(i2c_inst_t *bus);
 
 private:
+    i2c_inst_t *mI2C1;
     WS2812 mNeopixel;
+    SSD1306 mDisplay;
 };
 
 #endif // RP2040_APPLICATION_H
--- src/application.cpp
+++ src/application.cpp
@@ -4,20 +4,27 @@ const uint32_t Application::neopixel_pin = 16;
 const uint32_t Application::neopixel_num_leds = 1;
 const uint32_t Application::neopixel_max_brightness = 25;
 
+const uint32_t Application::i2c1_sda_pin  = 26;
+const uint32_t Application::i2c1_scl_pin  = 27;
+
 const uint32_t Application::heartbeat_ms = 1000;
+
+#define I2C_BUS_SPEED_KHZ(x) x * 1000
 
 /**
  * @brief Construct a new Application:: Application object
  * 
  */
 Application::Application()
-    : mNeopixel(
+    : mI2C1(i2c1)
+    , mNeopixel(
         neopixel_pin,
         neopixel_num_leds,
         pio0,
         0,
         WS2812::DataFormat::FORMAT_GRB
     )
+    , mDisplay(mI2C1)
 {
     initialize();
 }
@@ -60,4 +67,62 @@ void Application::initialize()
     sleep_ms(1000);
 
     printf("Booting...\n");
+    initializeI2C();
+    initializeDisplay();
+}
+
+void Application::initializeI2C()
+{
+    printf("Initializing I2C...\n");
+    i2c_init(mI2C1, I2C_BUS_SPEED_KHZ(1000));
+    gpio_set_function(i2c1_sda_pin, GPIO_FUNC_I2C);
+    gpio_set_function(i2c1_scl_pin, GPIO_FUNC_I2C);
+    gpio_pull_up(i2c1_sda_pin);
+    gpio_pull_up(i2c1_scl_pin);
+
+    i2cBusScan(mI2C1);
+}
+
+void Application::initializeDisplay()
+{
+    printf("Initializing display...\n");
+    mDisplay.initialize();
+
+    for(int i = 0; i < 3; i++) {
+        mDisplay.fill_screen(0xFF);
+        sleep_ms(500);
+        mDisplay.fill_screen(0x00);
+        sleep_ms(500);
+    }
+}
+
+void Application::i2cBusScan(i2c_inst_t *bus)
+{
+    uint8_t rxdata = 0;
+
+    printf("\nI2C Bus Scan\n");
+    printf("   0  1  2  3  4  5  6  7  8  9  A  B  C  D  E  F\n");
+
+    for (int addr = 0; addr < (1 << 7); ++addr) {
+        if (addr % 16 == 0) {
+            printf("%02x ", addr);
+        }
+
+        // Perform a 1-byte dummy read from the probe address. If a slave
+        // acknowledges this address, the function returns the number of bytes
+        // transferred. If the address byte is ignored, the function returns
+        // -1.
+
+        // Skip over any reserved addresses.
+        int ret;
+        uint8_t rxdata;
+        if (reserved_addr(addr))
+            ret = PICO_ERROR_GENERIC;
+        else
+            ret = i2c_read_blocking(bus, addr, &rxdata, 1, false);
+
+        printf(ret < 0 ? "." : "@");
+        printf(addr % 16 == 15 ? "\n" : "  ");
+    }
+    printf("Done.\n");
 }
